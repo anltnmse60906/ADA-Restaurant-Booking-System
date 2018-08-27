7f6b670c847c4b5d1b61e68bae13a3db1936cf9e
anltnmse60906 <anltnmse60906@fpt.edu.vn>
2018-08-27 19:10:04 +1000

    Update new home function just support for one restaurant

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -11,6 +11,16 @@ export class HomeComponent implements OnInit {
 
   model;
 
+  gridSize = 50;
+
+  topLeft = [0, 50, 100, 150, 200, 250];
+  topRight = [0, 50, 100, 150, 200, 250, 150, 200, 250];
+  barOpposite = [0, 50, 100, 150];
+  leftKitchen = [0, 50, 100, 150, 100, 150, 150, 100, 150, 150, 100, 150, 150, 100, 150];
+  rightKitchen = [0, 50, 100,];
+
+  tables = ["table 1", "table 2"];
+  selectedTbl;
 
   constructor(private router: Router,
               private routes: ActivatedRoute,
@@ -21,9 +31,31 @@ export class HomeComponent implements OnInit {
   }
 
   onFindRestaurant() {
-    this.router.navigate(['restaurants'], {relativeTo: this.routes})
+    // this.router.navigate(['restaurants'], {relativeTo: this.routes})
+    this.router.navigate(['/restaurants', 1]);
   }
+
   selectToday() {
     this.model = this.calendar.getToday();
   }
+
+  onStart(event) {
+    console.log('started output:', event);
+    console.log(event.x);
+    console.log(event.y);
+  }
+
+  onStop(event) {
+    console.log('stopped output:', event);
+    console.log(event.x);
+    console.log(event.y);
+  }
+
+  onClick(event, tbl: string) {
+    console.log('stopped output:', event);
+    console.log(event.x);
+    console.log(event.y);
+    this.selectedTbl = tbl;
+  }
+
 }
