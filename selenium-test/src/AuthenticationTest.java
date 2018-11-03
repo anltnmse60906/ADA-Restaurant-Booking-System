8b0ef3569d28d5ea1f0b37fc3865140f20e0e5bb
anltnmse60906 <anltnmse60906@fpt.edu.vn>
2018-11-03 19:48:34 +1100

    Add pattern to Config file

--- selenium-test/src/AuthenticationTest.java
+++ selenium-test/src/AuthenticationTest.java
@@ -17,21 +17,21 @@ public class AuthenticationTest{
     @Test(priority = 0, groups = {"authentication"})
     public void testSignupFunction() {
         driver.get(Config.host + "signup");
-        WebElement email=driver.findElement(By.cssSelector("#email"));
+        WebElement email=driver.findElement(By.cssSelector(Config.email));
         int randomNumber = (int) (Math.random()*1000);
         emailStr = new StringBuilder("user01").append(randomNumber).append("@student.uts.edu.au").toString();
         email.sendKeys(emailStr);
 
-        WebElement password =driver.findElement(By.cssSelector("#password"));
+        WebElement password =driver.findElement(By.cssSelector(Config.password));
         password.sendKeys("test_0111111");
 
-        WebElement firstName =driver.findElement(By.cssSelector("#firstName"));
+        WebElement firstName =driver.findElement(By.cssSelector(Config.firstName));
         firstName.sendKeys("Tran Nhut");
 
-        WebElement lastName =driver.findElement(By.cssSelector("#lastName"));
+        WebElement lastName =driver.findElement(By.cssSelector(Config.lastName));
         lastName.sendKeys("Le");
 
-        WebElement phoneNumber =driver.findElement(By.cssSelector("#phoneNumber"));
+        WebElement phoneNumber =driver.findElement(By.cssSelector(Config.phoneNumber));
         phoneNumber.sendKeys("33387432");
 
         WebElement submitBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-signup/div/div/div/div[2]/section/div/div/div/div[2]/div/form/button"));
@@ -43,9 +43,9 @@ public class AuthenticationTest{
         } catch (Exception ex){
             System.out.println("Error while thread sleeping");
         }finally {
-            WebElement loginMsg = driver.findElement(By.cssSelector("#swal2-title"));
+            WebElement loginMsg = driver.findElement(By.cssSelector(Config.popupTitle));
             System.out.println(loginMsg.getText().trim());
-            Assert.assertEquals(loginMsg.getText().trim(),"Sign up Success");
+            Assert.assertEquals(loginMsg.getText().trim(),Config.signupSucessMsg);
         }
     }
 
@@ -53,19 +53,19 @@ public class AuthenticationTest{
     public void testSignupEmailDupplicatedFunction() {
 
         driver.get(Config.host + "signup");
-        WebElement email=driver.findElement(By.cssSelector("#email"));
+        WebElement email=driver.findElement(By.cssSelector(Config.email));
         email.sendKeys(emailStr);
 
-        WebElement password =driver.findElement(By.cssSelector("#password"));
+        WebElement password =driver.findElement(By.cssSelector(Config.password));
         password.sendKeys("test_0111111");
 
-        WebElement firstName =driver.findElement(By.cssSelector("#firstName"));
+        WebElement firstName =driver.findElement(By.cssSelector(Config.firstName));
         firstName.sendKeys("Tran Nhut");
 
-        WebElement lastName =driver.findElement(By.cssSelector("#lastName"));
+        WebElement lastName =driver.findElement(By.cssSelector(Config.lastName));
         lastName.sendKeys("Le");
 
-        WebElement phoneNumber =driver.findElement(By.cssSelector("#phoneNumber"));
+        WebElement phoneNumber =driver.findElement(By.cssSelector(Config.phoneNumber));
         phoneNumber.sendKeys("33387432");
 
         WebElement submitBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-signup/div/div/div/div[2]/section/div/div/div/div[2]/div/form/button"));
@@ -77,19 +77,19 @@ public class AuthenticationTest{
         } catch (Exception ex){
             System.out.println("Error while thread sleeping");
         }finally {
-            WebElement loginMsg = driver.findElement(By.cssSelector("#swal2-content"));
+            WebElement loginMsg = driver.findElement(By.cssSelector(Config.popupContent));
             System.out.println(loginMsg.getText().trim());
-            Assert.assertEquals(loginMsg.getText().trim(),"Email has been registered");
+            Assert.assertEquals(loginMsg.getText().trim(),Config.emailDupplicatedMsg);
         }
     }
 
     @Test(priority = 2,groups = {"authentication"})
     public void testLoginFunction() {
         driver.get(Config.host + "signin");
-        WebElement email=driver.findElement(By.cssSelector("#username"));
+        WebElement email=driver.findElement(By.cssSelector(Config.username));
         email.sendKeys(emailStr);
 
-        WebElement password =driver.findElement(By.cssSelector("#password"));
+        WebElement password =driver.findElement(By.cssSelector(Config.password));
         password.sendKeys("test_0111111");
 
         WebElement submitBtn = driver.findElement(By.cssSelector("#formLogin > button"));
@@ -101,9 +101,9 @@ public class AuthenticationTest{
         } catch (Exception ex){
             System.out.println("Error while thread sleeping");
         }finally {
-            WebElement loginMsg = driver.findElement(By.cssSelector("#swal2-content"));
+            WebElement loginMsg = driver.findElement(By.cssSelector(Config.popupContent));
             System.out.println(loginMsg.getText().trim());
-            Assert.assertEquals(loginMsg.getText().trim(),"Login success");
+            Assert.assertEquals(loginMsg.getText().trim(),Config.loginSucessMsg);
         }
     }
 
--- selenium-test/src/Config.java
+++ selenium-test/src/Config.java
@@ -1,3 +1,16 @@
 public class Config {
     public static String host = "localhost:4200/";
+    public static String email = "#email";
+    public static String username = "#username";
+    public static String password = "#password";
+    public static String firstName = "#firstName";
+    public static String lastName = "#lastName";
+    public static String phoneNumber = "#phoneNumber";
+
+    public static String popupTitle = "#swal2-title";
+    public static String popupContent = "#swal2-content";
+    public static String signupSucessMsg = "Sign up Success";
+    public static String emailDupplicatedMsg = "Email has been registered";
+    public static String loginSucessMsg = "Login success";
+
 }
