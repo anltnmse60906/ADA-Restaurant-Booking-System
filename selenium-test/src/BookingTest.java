265cdf0666a8cc3272803fb783cfcde05c6f1768
anltnmse60906 <anltnmse60906@fpt.edu.vn>
2018-11-03 19:37:13 +1100

    Add cancel booking for selenium automation testing

--- selenium-test/src/BookingTest.java
+++ selenium-test/src/BookingTest.java
@@ -12,41 +12,6 @@ public class BookingTest {
     private int retryCount = 0;
     private List<String> tableNumberLlt = new ArrayList<>();
 
-    public void selectTable() {
-        driver.get(Config.host);
-        int count = 0;
-        try {
-            Thread.sleep(2000);
-        } catch (Exception ex) {
-            System.out.println("Error while thread sleeping");
-        } finally {
-
-            // Get all tables on current date.
-            List<WebElement> topRightTables = driver.findElements(
-                    By.cssSelector("label.res-table"));
-            for (WebElement tblElement : topRightTables) {
-                try {
-                    tblElement.click();
-                    WebElement e = tblElement.findElement(By.cssSelector("app-table-element > label > div.selected"));
-                    if (e != null) {
-                        count++;
-                        WebElement tableNumber = tblElement.findElement(By.cssSelector("app-table-element > label > div.table-number"));
-                        tableNumberLlt.add(tableNumber.getText().trim());
-                    }
-                    // Just select the first three table for booking
-                    if (count == 3) {
-                        break;
-                    }
-                } catch (Exception e) {
-                    System.out.println("Do not have the disable div");
-                }
-            }
-        }
-        WebElement bookingBtn = driver.findElement(By.cssSelector("body > app-root > div > app-home > nav > button"));
-        bookingBtn.click();
-    }
-
-
     @Test(priority = 3, groups = {"booking"})
     public void reserveThreeTablesTest() {
         selectTable();
@@ -55,10 +20,13 @@ public class BookingTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-//            Check the table name on confirm-booking screen
+
+            //Check the table name on confirm-booking screen
             List<WebElement> listTables = driver.findElements(
                     By.cssSelector("body > app-root > div > app-confirm-booking > div > div > div.card-body > div.text-center.header-confirm-booking > div:nth-child(3) > div >*"));
             int cnt = 0;
+
+            // Compare the selected table from Home screen to confirm booking screen
             for (WebElement tblElement : listTables) {
                 for (String tblElementStr : tableNumberLlt) {
                     if (tblElement.getText().equals(tblElementStr)) {
@@ -72,6 +40,8 @@ public class BookingTest {
 
     @Test(priority = 4, groups = {"booking"})
     public void confirmBookingTest() {
+
+        // Find the confirm button.
         WebElement confirmBtn = driver.findElement(
                 By.cssSelector("body > app-root > div > app-confirm-booking > div > div > div.card-body > form > div:nth-child(4) > div.col.text-center.show > input"));
         confirmBtn.click();
@@ -80,6 +50,7 @@ public class BookingTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
+
             // Retry three time for getting the message from server
             WebElement successMsg = getSuccessMessage();
             Assert.assertNotNull(successMsg);
@@ -87,7 +58,7 @@ public class BookingTest {
     }
 
     @Test(priority = 5, groups = {"booking"})
-    public void cancelBookingTest(){
+    public void cancelBookingTest() {
         selectTable();
         try {
             Thread.sleep(3000);
@@ -100,25 +71,41 @@ public class BookingTest {
             List<WebElement> listTables = driver.findElements(
                     By.cssSelector("body > app-root > div > app-confirm-booking > div > div > div.card-body > div.text-center.header-confirm-booking > div:nth-child(3) > div >*"));
 
+            //Cast to string
+            List<String> tableNameList = new ArrayList<>();
+            for(WebElement e : listTables){
+                tableNameList.add(e.getText());
+            }
+
+            //Find cancel button
             WebElement cancelElement = driver.findElement(
                     By.cssSelector("body > app-root > div > app-confirm-booking > div > div > div.card-body > form > div:nth-child(4) > div.col.text-center.show > button"));
             cancelElement.click();
+
+            // Click confirm button on alert
             driver.switchTo().alert().accept();
 
-
-            // Get all tables on current date.
-
-            for (WebElement e: listTables){
-
-                WebElement topRightTable = driver.findElement(
-                        By.xpath("//app-table-element/label/div[text()='"+ e.getText() + "']"));
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // Get all tables on current date.
+                int count = 0;
+                for (String e : tableNameList) {
+                    try {
+                        // Check the reserved tables change the state to "not reserve" state.
+                        driver.findElement(
+                                By.xpath("//app-table-element/label/div[text()='" + e + "']/preceding-sibling::div[1 and not(@class='selected')]"));
+                        count++;
+                    } catch (Exception ex) {
+                        System.out.println("Table is selected");
+                    }
+                }
+                Assert.assertEquals(count, listTables.size());
             }
-
-
-
         }
     }
-
 
 
     @BeforeClass
@@ -131,8 +118,54 @@ public class BookingTest {
         }
     }
 
+    // Close browser after finishing the test
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-    private WebElement getSuccessMessage(){
+    private void selectTable() {
+        driver.get(Config.host);
+        int count = 0;
+        try {
+            Thread.sleep(2000);
+        } catch (Exception ex) {
+            System.out.println("Error while thread sleeping");
+        } finally {
+
+            // Get all tables on current date.
+            List<WebElement> allTables = driver.findElements(
+                    By.cssSelector("label.res-table"));
+            for (WebElement tblElement : allTables) {
+                try {
+                    tblElement.click();
+
+                    // Make sure that the table has change the css
+                    WebElement e = tblElement.findElement(By.cssSelector("app-table-element > label > div.selected"));
+
+                    // Get table number
+                    if (e != null) {
+                        count++;
+                        WebElement tableNumber = tblElement.findElement(By.cssSelector("app-table-element > label > div.table-number"));
+                        tableNumberLlt.add(tableNumber.getText().trim());
+                    }
+
+                    // Just select the first three table for booking
+                    if (count == 3) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Do not have the disable div");
+                }
+            }
+        }
+        WebElement bookingBtn = driver.findElement(By.cssSelector("body > app-root > div > app-home > nav > button"));
+        bookingBtn.click();
+    }
+
+    private WebElement getSuccessMessage() {
         do {
             try {
                 Thread.sleep(1000);
@@ -140,13 +173,14 @@ public class BookingTest {
                 e.printStackTrace();
             }
             try {
-                return  driver.findElement(
+                return driver.findElement(
                         By.cssSelector("body > app-root > div > app-confirm-booking > div > div > div.card-body > div.alert.alert-primary.show"));
-            } catch (Exception ex ){
+            } catch (Exception ex) {
                 retryCount++;
                 System.out.println("it goes to the catch loop: " + retryCount);
             }
-        }while (retryCount < 3 );
+        } while (retryCount < 3);
         return null;
     }
+
 }
